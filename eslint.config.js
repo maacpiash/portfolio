984326b5203a41f66459dc005ec98a210e2c230c
Ahad Chowdhury <ahad@maacpiash.com>
2024-05-06 19:59:29 +1000

    Fix ESLint config with Prettier!

--- eslint.config.js
+++ eslint.config.js
@@ -14,7 +14,7 @@ export default {
 			},
 			rules: {
 				// override/add rules settings here, such as:
-				"astro/no-set-html-directive": "error"
+				'astro/no-set-html-directive': 'error',
 			},
 		},
 	],
